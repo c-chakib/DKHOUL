dccda9be1cb60179f183bea9dd5fcc2c0e11f722
c-chakib <Chakib.CHEIKHI@hightech.edu>
2025-11-08 20:23:52 +0100

    fix(vercel): add post-build script to copy browser folder contents to dist root

---
+++ post-build.js
@@ -0,0 +1,22 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const browserDir = path.join(__dirname, 'frontend', 'dist', 'browser');
+const distDir = path.join(__dirname, 'frontend', 'dist');
+
+if (fs.existsSync(browserDir)) {
+  console.log('Copying browser contents to dist/...');
+  const files = fs.readdirSync(browserDir);
+  files.forEach(file => {
+    const src = path.join(browserDir, file);
+    const dest = path.join(distDir, file);
+    if (fs.lstatSync(src).isDirectory()) {
+      fs.cpSync(src, dest, { recursive: true });
+    } else {
+      fs.copyFileSync(src, dest);
+    }
+  });
+  console.log('Copy complete!');
+} else {
+  console.log('No browser directory found, skipping copy.');
+}
